0bc957d582974347f36b9636280db8ce66e44988
AyparaKerimova <eelify1@gmail.com>
2025-09-17 17:59:17 +0400

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,14 @@ const Navbar = () => {
       <header>
         <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
           <div className="flex h-16 items-center justify-between">
-            <div className="flex-1 md:flex md:items-center md:gap-12">
+            <div className="flex-1 flex items-center gap-12">
               <Link className="block " to="/">
                 <img width={140} height={300} src={Logo} alt="logo" />
               </Link>
             </div>
 
-            <div className="md:flex md:items-center md:gap-12">
-              <nav aria-label="Global" className="hidden md:block">
+            <div className="flex items-center gap-12">
+              <nav aria-label="Global" className="block">
                 <Link
                   className="text-gray-500 transition hover:text-gray-500/75"
                   to="/"
